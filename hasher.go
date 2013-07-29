12c0a167c334538131058b8a2da963d4e848b122
Chris Oei <code@oei.io>
2013-07-28 20:29:55 -0700

    Reformatted

--- hasher.go
+++ hasher.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-  "hash"
-  "hash/adler32"
-  "hash/crc32"
-  "crypto/md5"
-  "crypto/sha1"
-  "crypto/sha256"
-  "fmt"
-  "io"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
+	"hash"
+	"hash/adler32"
+	"hash/crc32"
+	"io"
 )
 
 type MultiHashContext struct {
-  contexts map[string]hash.Hash
+	contexts map[string]hash.Hash
 }
 
 func New() MultiHashContext {
-  contexts := make(map[string]hash.Hash)
-  contexts["adler32"] = adler32.New()
-  contexts["crc32"] = crc32.NewIEEE()
-  contexts["md5"] = md5.New()
-  contexts["sha1"] = sha1.New()
-  contexts["sha256"] = sha256.New()
-  return MultiHashContext{ contexts: contexts }
+	contexts := make(map[string]hash.Hash)
+	contexts["adler32"] = adler32.New()
+	contexts["crc32"] = crc32.NewIEEE()
+	contexts["md5"] = md5.New()
+	contexts["sha1"] = sha1.New()
+	contexts["sha256"] = sha256.New()
+	return MultiHashContext{contexts: contexts}
 }
 
 func (h *MultiHashContext) Writer() io.Writer {
-  var elements []io.Writer
-  for _, v := range h.contexts {
-    elements = append(elements, v)
-  }
-  return io.MultiWriter(elements...)
+	var elements []io.Writer
+	for _, v := range h.contexts {
+		elements = append(elements, v)
+	}
+	return io.MultiWriter(elements...)
 }
 
 func (h *MultiHashContext) Result() map[string]string {
-  result := make(map[string]string)
-  for k, v := range h.contexts {
-    result[k] = fmt.Sprintf("%x", v.Sum(nil))
-  }
-  return result
+	result := make(map[string]string)
+	for k, v := range h.contexts {
+		result[k] = fmt.Sprintf("%x", v.Sum(nil))
+	}
+	return result
 }
 
 func main() {
-  h := New()
-  w := h.Writer()
-  fmt.Fprintf(w, "Hello, world!")
-  fmt.Printf("%v", h.Result())
+	h := New()
+	w := h.Writer()
+	fmt.Fprintf(w, "Hello, world!")
+	fmt.Printf("%v", h.Result())
 }
