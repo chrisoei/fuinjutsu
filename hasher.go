a2e9a94b443a21e8df93be5fdde4af267b3c7f5e
Chris Oei <code@oei.io>
2013-08-19 22:17:57 -0700

    Revamped. Seems to work.

--- hasher.go
+++ hasher.go
@@ -1,49 +1,90 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
+	"database/sql"
+	"encoding/json"
+	"flag"
 	"fmt"
-	"hash"
-	"hash/adler32"
-	"hash/crc32"
-	"io"
+	"github.com/chrisoei/multidigest"
+	"github.com/chrisoei/oei"
+	_ "github.com/lib/pq"
+	"github.com/lib/pq"
+	"io/ioutil"
 )
 
-type MultiHashContext struct {
-	contexts map[string]hash.Hash
+func getDb() *sql.DB {
+	url := "postgres://lvqpwploovzuqi:qjTsEegr9Xx7pODFvsuVmAWG1l@ec2-54-221-224-138.compute-1.amazonaws.com:5432/d47n8kgim46830?sslmode=require"
+	cs, err := pq.ParseURL(url)
+	oei.ErrorHandler(err)
+	db, err := sql.Open("postgres", cs)
+	oei.ErrorHandler(err)
+	return db
 }
 
-func New() MultiHashContext {
-	contexts := make(map[string]hash.Hash)
-	contexts["adler32"] = adler32.New()
-	contexts["crc32"] = crc32.NewIEEE()
-	contexts["md5"] = md5.New()
-	contexts["sha1"] = sha1.New()
-	contexts["sha256"] = sha256.New()
-	return MultiHashContext{contexts: contexts}
+func storedData(data []byte) []byte {
+  if len(data) < 256 {
+    return data
+  }
+  return nil
 }
 
-func (h *MultiHashContext) Writer() io.Writer {
-	var elements []io.Writer
-	for _, v := range h.contexts {
-		elements = append(elements, v)
+func addAnnotation(db *sql.DB, hash_id int64, t string, a *string) {
+	if *a != "" {
+		_, err := db.Exec(`INSERT INTO annotations(hash_id, type, annotation) VALUES($1, $2, $3)`, hash_id, t, a);
+		oei.ErrorHandler(err)
 	}
-	return io.MultiWriter(elements...)
 }
 
-func (h *MultiHashContext) Result() map[string]string {
-	result := make(map[string]string)
-	for k, v := range h.contexts {
-		result[k] = fmt.Sprintf("%x", v.Sum(nil))
+func addProperty(db *sql.DB, hash_id int64, t string, p *string) {
+	if *p != "" {
+		_, err := db.Exec(`INSERT INTO properties(hash_id, type, property) VALUES($1, $2, $3)`, hash_id, t, p);
+		oei.ErrorHandler(err)
 	}
-	return result
 }
 
 func main() {
-	h := New()
+	db := getDb()
+	defer db.Close()
+
+	var fn = flag.String("file", "", "Filename to save")
+	var comment = flag.String("comment", "", "Comment")
+	var url = flag.String("url", "", "URL")
+	var rating = flag.String("rating", "", "Rating")
+
+	flag.Parse()
+
+	h := multidigest.New()
 	w := h.Writer()
-	fmt.Fprintf(w, "Hello, world!")
-	fmt.Printf("%v", h.Result())
+	data, err := ioutil.ReadFile(flag.Arg(0))
+	oei.ErrorHandler(err)
+	w.Write(data)
+	r := h.Result()
+	s, err := json.MarshalIndent(r, "", "  ")
+	fmt.Printf("%s\n", string(s))
+
+	_, err = db.Exec(`INSERT INTO hashes(bytes, adler32, crc32, md5, ripemd160, sha1, "sha2-256", "sha2-512", "sha3-256", ssdeep29, size, version) SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 WHERE NOT EXISTS (SELECT "sha2-256", "sha3-256" FROM hashes where "sha2-256" = $7 AND "sha3-256" = $9)`,
+		storedData(data),       // 1
+		r["adler32"],   // 2
+		r["crc32"],     // 3
+		r["md5"],       // 4
+		r["ripemd160"], // 5
+		r["sha1"],	// 6
+		r["sha2-256"],  // 7
+		r["sha2-512"],  // 8
+		r["sha3-256"],  // 9
+		r["ssdeep29"],  // 10
+		r["size"],      // 11
+		r["version"])   // 12
+	oei.ErrorHandler(err)
+
+	row := db.QueryRow(`SELECT id FROM hashes WHERE "sha2-256" = $1 AND "sha3-256" = $2`, r["sha2-256"], r["sha3-256"]);
+	var z int64
+	row.Scan(&z)
+	fmt.Printf("%d\n", z)
+	oei.ErrorHandler(err)
+
+	addAnnotation(db, z, "filename", fn)
+	addAnnotation(db, z, "comment", comment)
+	addAnnotation(db, z, "url", url)
+	addProperty(db, z, "rating", rating)
 }
---
+++ hashes.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS annotations;
+DROP TABLE IF EXISTS properties;
+DROP TABLE IF EXISTS hashes;
+
+CREATE TABLE hashes (
+  id SERIAL,
+  bytes BYTEA,
+  adler32 CHARACTER(8) NOT NULL CHECK(adler32 ~ '\A[0-9a-f]{8}\Z'),
+  crc32 CHARACTER(8) NOT NULL CHECK(crc32 ~ '\A[0-9a-f]{8}\Z'),
+  md5 CHARACTER(32) NOT NULL CHECK(md5 ~ '\A[0-9a-f]{32}\Z'),
+  ripemd160 CHARACTER(40) NOT NULL CHECK(ripemd160 ~ '\A[0-9a-f]{40}\Z'),
+  sha1 CHARACTER(40) NOT NULL CHECK(sha1 ~ '\A[0-9a-f]{40}\Z'),
+  "sha2-256" CHARACTER(64) NOT NULL CHECK("sha2-256" ~ '\A[0-9a-f]{64}\Z'),
+  "sha2-512" CHARACTER(128) NOT NULL CHECK("sha2-512" ~ '\A[0-9a-f]{128}\Z'),
+  "sha3-256" CHARACTER(64) NOT NULL CHECK("sha3-256" ~ '\A[0-9a-f]{64}\Z'),
+  ssdeep29 TEXT,
+  size BIGINT NOT NULL CHECK(size >= 0),
+  version TEXT NOT NULL CHECK(LENGTH(version) > 0)
+);
+
+CREATE UNIQUE INDEX ON hashes(id);
+CREATE UNIQUE INDEX no_duplicate_hashes ON hashes("sha2-256", "sha3-256");
+
+CREATE INDEX ON hashes(bytes);
+
+CREATE INDEX ON hashes(adler32);
+CREATE INDEX ON hashes(crc32);
+CREATE INDEX ON hashes(md5);
+CREATE INDEX ON hashes(ripemd160);
+CREATE INDEX ON hashes(sha1);
+CREATE INDEX ON hashes("sha2-256");
+CREATE INDEX ON hashes("sha2-512");
+CREATE INDEX ON hashes("sha3-256");
+CREATE INDEX ON hashes(size);
+
+DROP FUNCTION IF EXISTS binary_integrity(bytes BYTEA, mymd5 CHARACTER);
+CREATE FUNCTION binary_integrity(bytes BYTEA, mymd5 CHARACTER)
+RETURNS BOOLEAN
+AS
+$$
+DECLARE
+  result BOOLEAN;
+BEGIN
+  IF bytes IS NULL THEN
+    RETURN TRUE;
+  END IF;
+  IF LENGTH(bytes) = 0 THEN
+    RETURN TRUE;
+  END IF;
+  SELECT md5(bytes) = mymd5 INTO STRICT result;
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+ALTER TABLE hashes ADD CONSTRAINT bytes_integrity CHECK (binary_integrity(bytes, md5));
+
+CREATE TABLE annotations (
+  id SERIAL,
+  type TEXT,
+  annotation TEXT,
+  timestamp TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+  hash_id INTEGER NOT NULL REFERENCES hashes (id)
+);
+
+CREATE TABLE properties (
+  id SERIAL,
+  type TEXT,
+  property TEXT,
+  hash_id INTEGER NOT NULL REFERENCES hashes (id)
+);
+
+CREATE UNIQUE INDEX no_duplicate_properties ON properties(hash_id, type);
+
