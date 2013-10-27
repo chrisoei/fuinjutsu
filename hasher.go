04c9f07a7840b9c6f2087ac7d5b084c8e18cda7f
Chris Oei <code@oei.io>
2013-10-27 14:40:09 -0700

    Add -verify flag

--- hasher.go
+++ hasher.go
@@ -61,6 +61,25 @@ func addContents(db *sql.DB, hash_id int64, t []byte) {
 	}
 }
 
+func getHashId(filename string) string {
+	rg := regexp.MustCompile("\\[#(\\d+)\\]\\.\\w+")
+	hidF := ""
+	if rg.Match([]byte(filename)) {
+		hidF = rg.FindStringSubmatch(filename)[1]
+	}
+	hidXbytes, err := xattr.Get(filename, "user.io.oei.hash_id")
+	hidX := string(hidXbytes)
+	if err == nil {
+		if hidF != "" && hidF != hidX {
+			return ""
+		} else {
+			return hidX
+		}
+	} else {
+		return hidF
+	}
+}
+
 func hashFile(filename string, save bool) (map[string]string, []byte) {
 	h := multidigest.New()
 	w := h.Writer()
@@ -102,6 +121,7 @@ func main() {
 	var save = flag.Bool("save", false, "Save file in database")
 	var useXattr = flag.Bool("xattr", false, "Save hash ID in extended attributes")
 	var rename = flag.Bool("rename", false, "Rename the file using the hash ID as part of the filename")
+	var verify = flag.Bool("verify", false, "Verify that the contents of the file have not changed")
 
 	flag.Parse()
 
@@ -117,59 +137,75 @@ func main() {
 			fmt.Printf("%s\n", string(s))
 		}
 
-		_, err := db.Exec(`INSERT INTO hashes(adler32, crc32, md5, ripemd160, sha1, "sha2-256", "sha2-512", "sha3-256", ssdeep29, size, version) SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 WHERE NOT EXISTS (SELECT "sha2-256", "sha3-256" FROM hashes where "sha2-256" = $6 AND "sha3-256" = $8)`,
-			r["adler32"],   // 1
-			r["crc32"],     // 2
-			r["md5"],       // 3
-			r["ripemd160"], // 4
-			r["sha1"],      // 5
-			r["sha2-256"],  // 6
-			r["sha2-512"],  // 7
-			r["sha3-256"],  // 8
-			r["ssdeep29"],  // 9
-			r["size"],      // 10
-			r["version"])   // 11
-		oei.ErrorHandler(err)
+		if *verify {
+			hid := getHashId(filename)
+			if hid == "" {
+				fmt.Printf("UNKNOWN: %s\n", filename)
+			} else {
+				row := db.QueryRow(`SELECT "sha2-256" FROM hashes where id = $1`, hid)
+				var sha256 string
+				row.Scan(&sha256)
+				if sha256 == r["sha2-256"] {
+					fmt.Printf("OK: %s\n", filename)
+				} else {
+					fmt.Printf("ERROR: %s\n", filename)
+				}
+			}
+		} else {
+			_, err := db.Exec(`INSERT INTO hashes(adler32, crc32, md5, ripemd160, sha1, "sha2-256", "sha2-512", "sha3-256", ssdeep29, size, version) SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 WHERE NOT EXISTS (SELECT "sha2-256", "sha3-256" FROM hashes where "sha2-256" = $6 AND "sha3-256" = $8)`,
+				r["adler32"],   // 1
+				r["crc32"],     // 2
+				r["md5"],       // 3
+				r["ripemd160"], // 4
+				r["sha1"],      // 5
+				r["sha2-256"],  // 6
+				r["sha2-512"],  // 7
+				r["sha3-256"],  // 8
+				r["ssdeep29"],  // 9
+				r["size"],      // 10
+				r["version"])   // 11
+			oei.ErrorHandler(err)
 
-		row := db.QueryRow(`SELECT id FROM hashes WHERE "sha2-256" = $1 AND "sha3-256" = $2`, r["sha2-256"], r["sha3-256"])
-		var z int64
-		row.Scan(&z)
-		hid := fmt.Sprintf("%d", z)
-		if *useXattr {
-			oei.ErrorHandler(xattr.Set(filename, "user.io.oei.hash_id", []byte(hid)))
-		}
-		if oei.Verbosity() >= 0 {
-			fmt.Println(hid)
-		}
-		if *rename {
-			newFilename := hashName(filename, z)
-			if newFilename != filename {
-				os.Rename(filename, newFilename)
-				filename = newFilename
+			row := db.QueryRow(`SELECT id FROM hashes WHERE "sha2-256" = $1 AND "sha3-256" = $2`, r["sha2-256"], r["sha3-256"])
+			var z int64
+			row.Scan(&z)
+			hid := fmt.Sprintf("%d", z)
+			if *useXattr {
+				oei.ErrorHandler(xattr.Set(filename, "user.io.oei.hash_id", []byte(hid)))
 			}
-		}
+			if oei.Verbosity() >= 0 {
+				fmt.Println(hid)
+			}
+			if *rename {
+				newFilename := hashName(filename, z)
+				if newFilename != filename {
+					os.Rename(filename, newFilename)
+					filename = newFilename
+				}
+			}
 
-		switch *fnOverride {
-		case "*":
-			{
-				fn = filename
+			switch *fnOverride {
+			case "*":
+				{
+					fn = filename
+				}
+			case "":
+				{
+					fn = ""
+				}
+			default:
+				{
+					fn = *fnOverride
+				}
 			}
-		case "":
-			{
-				fn = ""
-			}
-		default:
-			{
-				fn = *fnOverride
-			}
-		}
 
-		addAnnotation(db, z, "filename", &fn)
-		addAnnotation(db, z, "comment", comment)
-		addAnnotation(db, z, "url", url)
-		addProperty(db, z, "rating", rating)
-		addProperty(db, z, "imdb", imdb)
-		addTag(db, z, tag)
-		addContents(db, z, data)
+			addAnnotation(db, z, "filename", &fn)
+			addAnnotation(db, z, "comment", comment)
+			addAnnotation(db, z, "url", url)
+			addProperty(db, z, "rating", rating)
+			addProperty(db, z, "imdb", imdb)
+			addTag(db, z, tag)
+			addContents(db, z, data)
+		}
 	}
 }
